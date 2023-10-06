4abe59a24daad3388b50c89c9ab05a6f0bb8d1c2
jhonkw9w8e <133713944+jhonkw9w8e@users.noreply.github.com>
2023-10-06 09:47:20 -0500

    Calculadora

---
+++ Calculadora 3.py
@@ -0,0 +1,11 @@
+#Programa que pasara dos valores enteros por teclado, despues calculara la suma, resta, multiplicacion, division y usara un metodo para cada una e imprimir los resultados obtenidos
+
+#CODIGO PARA DEFINIR LAS OPERACIONES ARITMETICAS
+def __init__(self, suma, resta, multiplicacion, division):
+    self.suma=int()
+    self.resta=int()
+    self.multiplicacion=int()
+    self.division=int()
+
+
+
